7e13debe0722ef658e27db6a9d015cf5ed534074
JesselZapanta <jesselzapanta9@gmail.com>
2025-05-13 18:43:47 +0800

    update repo

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,11 @@ export default defineConfig({
         }),
         react(),
     ],
-    server: {
-        host: true,
-        port: 3000,
-        hmr: {
-            host: "192.168.123.4", // IPv4 Address.
-        },
-    },
+    // server: {
+    //     host: true,
+    //     port: 3000,
+    //     hmr: {
+    //         host: "192.168.123.4", // IPv4 Address.
+    //     },
+    // },
 });
